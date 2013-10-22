c39dadab4c751698d49cd1797b374c1ebe2ec0de
Damon Oehlman <damon.oehlman@gmail.com>
2013-10-22 22:07:18 +1100

    Added initial next implementation

---
+++ next.js
@@ -0,0 +1,50 @@
+/* jshint node: true */
+'use strict';
+
+/**
+  ### next
+
+  ```
+  f(name, el) => fn
+  ```
+
+  The `next` function is used to pull event data from `el` for the event
+  named `name`.  This can be useful when combined with a
+  [pull-stream](https://github.com/dominictarr/pull-stream) to capture
+  a stream of events from a DOM elements.
+
+  <<< examples/next.js
+**/
+module.exports = function(name, el) {
+  var buffer = [];
+  var next = [];
+
+  function handleEvent(evt) {
+    next.length ? next.shift()(null, evt) : buffer[buffer.length] = evt;
+  }
+
+  // add the event listener to the object
+  el.addEventListener(name, handleEvent);
+
+  // return the function for getting the event
+  return function(end, cb) {
+    // handle the non pull-stream case of a single argument
+    if (typeof end == 'function') {
+      cb = end;
+      end = false;
+    }
+
+    // if we are ending the stream, then remove the listener
+    if (end) {
+      el.removeEventListener(name, handleEvent);
+      return cb(end);
+    }
+
+    if (buffer.length > 0) {
+      return cb(null, buffer.shift());
+    }
+
+    // otherwise, save the cb
+    next = [ cb ];
+  };
+};
